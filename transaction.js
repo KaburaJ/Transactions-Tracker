53f618b076043dab4b74c579b2c0dade277f4973
Joan Kabura <kaburajoan25@gmail.com>
2023-05-18 09:36:48 -0700

    transaction tracking version 2

---
+++ transaction.js
@@ -0,0 +1,78 @@
+const addTransactionBtn = document.getElementById("add-transaction");
+const transactionList = document.getElementById("transaction-list");
+
+addTransactionBtn.addEventListener("click", addTransaction);
+
+function addTransaction() {
+  const titleInput = document.getElementById("transaction-title");
+  const categoryInput = document.getElementById("transaction-category");
+  const amountInput = document.getElementById("transaction-amount");
+
+  const title = titleInput.value;
+  const category = categoryInput.value;
+  const amount = parseFloat(amountInput.value);
+
+  if (title.trim() === "" || isNaN(amount)) {
+    alert("Please enter valid transaction details");
+    return;
+  }
+
+  const listItem = document.createElement("li");
+  listItem.classList.add("transaction-list-item");
+  listItem.classList.add(category);
+
+  listItem.innerHTML = `
+    <span>${title}</span>
+    <span>${category === "revenue" ? "+" : "-"}${amount}</span>
+    <button class="delete-transaction">Delete</button>
+  `;
+
+  transactionList.appendChild(listItem);
+
+  titleInput.value = "";
+  categoryInput.value = "revenue";
+  amountInput.value = "";
+
+  updateLocalStorage();
+}
+
+transactionList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete-transaction")) {
+    event.target.parentNode.remove();
+    updateLocalStorage();
+  }
+});
+
+function updateLocalStorage() {
+  const transactions = Array.from(transactionList.children).map((item) => ({
+    title: item.children[0].textContent,
+    category: item.classList.contains("revenue") ? "revenue" : "expense",
+    amount: parseFloat(item.children[1].textContent),
+  }));
+
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+
+function loadTransactions() {
+    const transactions = JSON.parse(localStorage.getItem("transactions"));
+  
+    if (transactions) {
+      transactions.forEach((transaction) => {
+        const listItem = document.createElement("li");
+        listItem.classList.add("transaction-list-item");
+        listItem.classList.add(transaction.category);
+  
+        listItem.innerHTML = `
+          <span>${transaction.title}</span>
+          <span>${transaction.category === "revenue" ? "+" : "-"}${transaction.amount}</span>
+          <button class="delete-transaction">Delete</button>
+        `;
+  
+        transactionList.appendChild(listItem);
+      });
+    }
+  }
+  
+
+loadTransactions();
